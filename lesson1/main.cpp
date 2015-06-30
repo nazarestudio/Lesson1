3b187b3b69480368414ad16f317efbd4a64c7f95
nazarestudio <nazarestudio@gamil.com>
2015-06-30 17:38:45 +0300

    MASAGE

--- lesson1/main.cpp
+++ lesson1/main.cpp
@@ -2,19 +2,27 @@
 
 using namespace std;
 
-void task_example()
+void task_exmple()
 {
 	cout << "Output 2-digit number line by line\nInput 2-digit number: ";
 	int number;
 	cin >> number;
 
 	cout << "First number: " << number / 10 << endl;
-	cout << "Second number: " << number % 10 << endl;
+	cout << "Fifth number: " << number %10 << endl;
 }
 
 void task1()
 {
-	cout << "Write solution for given task here" << endl;
+	cout << "Output 5-digit number line by line\nInput 5-digit number: ";
+	int number;
+	cin >> number;
+
+	cout << "First number: " << number / 10000 << endl;
+	cout << "Second numser:" << (number % 10000 - number %1000) /1000 << endl;
+	cout << "Third number: " << (number % 1000 - number %100) /100 << endl;
+	cout << "Fourth number: " << (number % 100 - number %10) /10 << endl;
+	cout << "Fifth number: " << number %10 << endl;
 }
 
 int main(int argc, char *argv[])
