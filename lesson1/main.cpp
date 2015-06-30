87d97cf08e3b2dd58fdb1c6a872cc4e96013712d
nazarestudio <nazarestudio@gamil.com>
2015-06-30 13:11:53 +0300

    Initial commit

---
+++ lesson1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void task1()
+{
+	cout << "Write solution for given task here" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+	cout << "Write code, bleat'!!!!" << endl;
+	
+	task1();
+
+	system("pause");
+	return 0;
+}
