03ce3e80c0bbd133902247b9cb0d5f7c290ff21c
nazarestudio <nazarestudio@gamil.com>
2015-07-01 18:19:43 +0300

    1 task

--- lesson1/main.cpp
+++ lesson1/main.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
 
 using namespace std;
+int main(int argc, char* argv[])
 
-void task_exmple()
-{
-	cout << "Output 2-digit number line by line\nInput 2-digit number: ";
-	int number;
-	cin >> number;
-
-	cout << "First number: " << number / 10 << endl;
-	cout << "Fifth number: " << number %10 << endl;
-}
-
-void task1()
 {
 	cout << "Output 5-digit number line by line\nInput 5-digit number: ";
 	int number;
@@ -23,14 +13,6 @@ void task1()
 	cout << "Third number: " << (number % 1000 - number %100) /100 << endl;
 	cout << "Fourth number: " << (number % 100 - number %10) /10 << endl;
 	cout << "Fifth number: " << number %10 << endl;
-}
-
-int main(int argc, char *argv[])
-{
-	cout << "Write code, bleat'!!!!" << endl;
-	
-	task_example();
-
 	system("pause");
 	return 0;
 }
