cc2ef1d2ba899d8d22ae2282c197e2b9d4d99b5a
nazarestudio <nazarestudio@gamil.com>
2015-07-02 15:16:15 +0300

    equestion

--- lesson1/main.cpp
+++ lesson1/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+ï»¿#include "iostream"
+#include "cmath"
 
 using namespace std;
-int main(int argc, char* argv[])
 
-{
+int main ( int argc, char* argv[])
+
+/*{
 	cout << "Output 5-digit number line by line\nInput 5-digit number: ";
 	int number;
 	cin >> number;
@@ -13,12 +15,15 @@ int main(int argc, char* argv[])
 	cout << "Third number: " << (number % 1000 - number %100) /100 << endl;
 	cout << "Fourth number: " << (number % 100 - number %10) /10 << endl;
 	cout << "Fifth number: " << number %10 << endl;
-	
-	//task 1_2 Èç ïÿòè îòäåëüíûõ ÷èñåë ñîáðàòü îäíî 5-òè çíà÷íîå
+	system ("pause");
+	 return 0;
+	 }*/
+	//task 1_2 ÃˆÃ§ Ã¯Ã¿Ã²Ã¨ Ã®Ã²Ã¤Ã¥Ã«Ã¼Ã­Ã»Ãµ Ã·Ã¨Ã±Ã¥Ã« Ã±Ã®Ã¡Ã°Ã Ã²Ã¼ Ã®Ã¤Ã­Ã® 5-Ã²Ã¨ Ã§Ã­Ã Ã·Ã­Ã®Ã¥
+{
 	 cout << endl;
 	 cout << "task1_2, formed 5 digit number from 5 another numbers. " << endl;
 	 cout << "Enter numbers:" << endl;
-	 int a, b, c, d, e;
+	 int a, b, c, d, e, result;
 	 cout << "- 1st numer:";
 	 cin >> a;
 	 cout << "- 2nd numer:";
@@ -34,6 +39,40 @@ int main(int argc, char* argv[])
 	 return 0;
      
  }
+/*{
+	double d, b, a, c, x1, x2; 
+	cout << "ax^2+bx+c=0" << endl;
+	cout << "enter a:";
+	cin >> a; 
+	cout<<"enter b:";
+	cin>>b; 
+	cout<< "enter c:";
+	cin>>c; 
+	d=b*b-4*a*c;
+	cout<<"D="<< d << endl;
+	if (d>0)
+	{
+      x1=(-b+sqrt(d))/2*a;
+	  x2=(-b-sqrt(d))/2*a;
+	  cout<< "x1=(-b+sqrt(d))/2a="<<x1<<endl;
+	  cout<< "x2=(-b-sqrt(d))/2a="<<x2<<endl;
+	}
+	    else 
+	         {
+		      if (d==0)
+		       {
+			     x1=(-b+sqrt(d))/2*a;
+			     cout<< "x=(-b+sqrt(d))/2a="<<x1<<endl;
+		                                                }
+		else 
+		    {
+		     if (d<0)
+		       {
+			    cout<<"equation without solutions"<<endl;
+		                                                 }
+		     }
+	}
 
-
- 
+	system ("pause");
+	return 0;
+}*/
